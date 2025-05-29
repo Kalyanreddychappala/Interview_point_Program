d8e3ba42db6e07607966a4ca259b9bff90ff57c3
Your Name <your-email@example.com>
2025-05-29 20:56:01 +0530

    interview-point

--- practice/Practice.java
+++ practice/Practice.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public class Practice {
 
@@ -17,6 +18,10 @@ public class Practice {
 			.skip(1)
 			.findFirst();
 	System.out.println(second);
+	Stream.iterate(new int[]{0, 1}, f -> new int[]{f[1], f[0] + f[1]})
+    .map(f -> f[0])
+    .limit(10)
+    .forEach(n -> System.out.print(n + " "));
 			
 		}
 	}
